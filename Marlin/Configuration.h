0b22b8bb94eb11d5fc308807b82d8f90210442aa
SilentBobIsCoding <necr@tut.by>
2015-12-19 17:15:33 +0300

    Update config after installing bowden extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -334,11 +334,11 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 #define max_software_endstops true  // If true, axis won't move to coordinates greater than the defined lengths below.
 
 // Travel limits after homing
-#define X_MAX_POS 150
+#define X_MAX_POS 200
 #define X_MIN_POS 0
-#define Y_MAX_POS 160
+#define Y_MAX_POS 200
 #define Y_MIN_POS 0
-#define Z_MAX_POS 75
+#define Z_MAX_POS 110
 #define Z_MIN_POS 0
 
 #define X_MAX_LENGTH (X_MAX_POS - X_MIN_POS)
@@ -522,8 +522,8 @@ const bool Z_MAX_ENDSTOP_INVERTING = true; // set to true to invert the logic of
 //#define EEPROM_CHITCHAT
 
 // Preheat Constants
-#define PLA_PREHEAT_HOTEND_TEMP 220
-#define PLA_PREHEAT_HPB_TEMP 55
+#define PLA_PREHEAT_HOTEND_TEMP 200
+#define PLA_PREHEAT_HPB_TEMP 50
 #define PLA_PREHEAT_FAN_SPEED 255   // Insert Value between 0 and 255
 
 #define ABS_PREHEAT_HOTEND_TEMP 240
